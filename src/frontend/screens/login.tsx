e3df8fccbab09ef1102a3fdb52345cf4472aac92
Rafael Ouverney <rafael_ouverney31@hotmail.com>
2025-08-05 20:47:36 -0300

    [FIRST COMMIT] Projeto criado

---
+++ src/frontend/screens/login.tsx
@@ -0,0 +1,11 @@
+const Login = () => {
+    return (
+        <div className="flex items-center justify-center min-h-screen">
+            <div className="flex flex-col items-center justify-center w-1/2 h-1/2 p-8">
+                <img src="./src/assets/login-image.png" alt="Login Image" className="md:h-1/2 md:w-1/2 " />
+            </div>
+        </div>
+    );
+}
+
+export default Login;
